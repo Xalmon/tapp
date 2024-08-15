c51c2e70e1bce276ad305255318c97dd47982924
Xalmon <xalmonoyefule@gmail.com>
2024-08-15 15:41:43 +0100

    create a springboot project or bash, push to git and auto create branches and push to dockerhub

---
+++ src/main/java/com.example.tapp/tappApplication.java
@@ -0,0 +1,12 @@
+package com.example.tapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(tappApplication.class, args);
+  }
+}
---
+++ src/test/java/com.example.tapp/tappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class tappApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
